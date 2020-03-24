b83bba6b4b9986d807b81205e30d7e5c57b314e6
Andrew Bell <saltdrew@gmail.com>
2020-03-24 12:53:19 +0000

    Button Class added
    Button class to replace "difficulty button" with additional constructor for buttons that are images.

---
+++ MathsGame/src/app/Button.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.border.Border;
+
+public class Button extends JButton{
+
+	private static final long serialVersionUID = 1L;
+
+	private Font font = new Font(Font.SERIF,Font.PLAIN,35);
+	private Color color;
+	private Color bright;
+	private Color dim;
+	
+	private Border border = BorderFactory.createLineBorder(Color.BLACK);
+	public Button(ImageIcon icon){
+		super();
+		this.setOpaque(false); 
+		this.setContentAreaFilled(false);
+		this.setBorder(null); 
+		this.setIcon(icon);
+	
+	}
+	public Button(String text, Color color){
+		super(text);
+		bright = color.brighter().brighter();
+		dim= color.darker().darker();
+		this.setBackground(dim);
+		this.setBorder(border);
+		this.setFont(font);
+	}
+	public void changeBrightness(boolean brighter) {
+		if (brighter) {
+			color=bright;
+		}
+		else {
+			color=dim;
+		}
+		this.setBackground(color);
+	}
+	
+}
