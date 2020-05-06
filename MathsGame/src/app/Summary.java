76d6eed75a6eb1ec13a7f596fa13a60baa214596
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2020-05-06 13:19:05 +0100

    Spelling correction

--- MathsGame/src/app/Summary.java
+++ MathsGame/src/app/Summary.java
@@ -37,7 +37,7 @@ public class Summary extends Window{
 		scoreText = new JLabel("Score: "); scoreText.setFont(GFonts.mediumFont);
 		correctText= new JLabel("Correct Answers: ");correctText.setFont(GFonts.mediumFont);
 		incorrectText= new JLabel("Incorrect Answers: ");incorrectText.setFont(GFonts.mediumFont);
-		titleText= new JLabel("Game Sumarry");titleText.setFont(GFonts.titleFont);
+		titleText= new JLabel("Game Summary");titleText.setFont(GFonts.titleFont);
 		centerPanel.add(titleText);
 		centerPanel.add(Box.createRigidArea(new Dimension(0,30)));
 		centerPanel.add(scoreText);
